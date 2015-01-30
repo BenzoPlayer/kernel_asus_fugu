912890cf0887363d060908b1aa1053efe929fd27
Patrick Tjin <pattjin@google.com>
2015-01-30 10:56:34 -0800

    hdmi_audio: fix valid bit setting
    Change the valid bit to 0 (logical TRUE).  This fixes
    HDMI audio on certain Vizio TVs.
    
    Bug: 18377970
    
    Signed-off-by: Patrick Tjin <pattjin@google.com>

--- drivers/external_drivers/intel_media/hdmi_audio/intel_mid_hdmi_audio.c
+++ drivers/external_drivers/intel_media/hdmi_audio/intel_mid_hdmi_audio.c
@@ -515,7 +515,7 @@ int snd_intelhad_prog_audio_ctrl_v2(struct snd_pcm_substream *substream,
 	else
 		cfg_val.cfg_regx_v2.layout = LAYOUT1;
 
-	cfg_val.cfg_regx_v2.val_bit = 1;
+	cfg_val.cfg_regx_v2.val_bit = 0;
 	had_write_register(AUD_CONFIG, cfg_val.cfg_regval);
 	return 0;
 }
@@ -571,7 +571,7 @@ int snd_intelhad_prog_audio_ctrl_v1(struct snd_pcm_substream *substream,
 
 	}
 
-	cfg_val.cfg_regx.val_bit = 1;
+	cfg_val.cfg_regx.val_bit = 0;
 	had_write_register(AUD_CONFIG, cfg_val.cfg_regval);
 	return 0;
 }
